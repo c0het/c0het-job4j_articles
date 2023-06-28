936f3f268a6379e898f22824652bb47c53316347
c0het <c0het401@gmail.com>
2023-06-28 20:35:39 +0300

    3. Что такое TDD? [#4918 # [#4918]]

--- src/test/java/ru/job4j/template/Generator1Test.java
+++ src/test/java/ru/job4j/template/Generator1Test.java
@@ -1,50 +1,52 @@
 package ru.job4j.template;
 
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+@Disabled
 class Generator1Test {
 
-    Map<String, String> args = new HashMap<>();
-    String template = new String();
-
-    @Ignore
+    @Test
     public void whenTemplateDontHaveKeys() {
         Generator generator = new Generator1();
+        Map<String, String> args = new HashMap<>();
+        String template =  "I am  name, Who are subject?";
         args.put("name", "ivan");
         args.put("subject", "you");
-        template = "I am  name, Who are subject?";
         assertThatThrownBy(() -> generator.produce(template, args)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
+    @Test
     public void whenTempalateHasUnknowKeys() {
         Generator generator = new Generator1();
-        template = "I am  ${name}, Who are ${subject}?I am from ${city}";
+        Map<String, String> args = new HashMap<>();
+        String template = "I am  ${name}, Who are ${subject}?I am from ${city}";
         args.put("name", "ivan");
         args.put("subject", "you");
         assertThatThrownBy(() -> generator.produce(template, args)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
+    @Test
     public void whenMapHasUnknowKeys() {
         Generator generator = new Generator1();
-        template = "I am ${name}, Who are ${subject}";
+        Map<String, String> args = new HashMap<>();
+        String template = "I am ${name}, Who are ${subject}";
         args.put("name", "ivan");
         args.put("subject", "you");
         args.put("city", "Moscow");
         assertThatThrownBy(() -> generator.produce(template, args)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Ignore
+    @Test
     public void whenWorks() {
+        Map<String, String> args = new HashMap<>();
+        String template = "I am ${name}, Who are ${subject}";
         Generator generator = new Generator1();
-        template = "I am ${name}, Who are ${subject}";
         args.put("name", "ivan");
         args.put("subject", "you");
         assertThat("I am ivan, Who are you").isEqualTo(generator.produce(template, args));
