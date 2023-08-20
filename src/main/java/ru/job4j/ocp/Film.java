33263f66162e40224d6bd068b17b91218e072486
c0het <c0het401@gmail.com>
2023-08-20 16:39:28 +0300

    0. Принцип открытости закрытости [#4914]

---
+++ src/main/java/ru/job4j/ocp/Film.java
@@ -0,0 +1,8 @@
+package ru.job4j.ocp;
+
+public class Film {
+
+    public void copy() {
+        System.out.println("Film copy");
+    }
+}
---
+++ src/main/java/ru/job4j/ocp/PaperDocument.java
@@ -0,0 +1,9 @@
+package ru.job4j.ocp;
+
+final public class PaperDocument {
+
+    public void copy() {
+        System.out.println("Договор копируюется");
+    }
+
+}
